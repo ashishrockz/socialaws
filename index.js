0810bc605d76ba1bc6665b08b7b1166142a222d2
B CHUCHEL ASHISH <85539405+ashishrockz@users.noreply.github.com>
2024-11-19 05:48:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ app.use((req, res) => {
 });
 
 // Start server
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}...`);
 });
