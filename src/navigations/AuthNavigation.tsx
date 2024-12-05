a73ad834afef64b01ffcbbe1ffbadf7bbe20032a
B CHUCHEL ASHISH <85539405+ashishrockz@users.noreply.github.com>
2024-12-05 18:15:12 +0530

    dfs

--- src/navigations/AuthNavigation.tsx
+++ src/navigations/AuthNavigation.tsx
@@ -1,12 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Login from '../screens/login/Login'
+import AddEmployee from '../screens/addEmployee/AddEmployee'
 const AuthNavigation = () => {
   return (
     <View>
-      <Login/>
+      {/* <Login/> */}
+      <AddEmployee/>
     </View>
   )
 }
-
 export default AuthNavigation
--- src/screens/login/Login.tsx
+++ src/screens/login/Login.tsx
@@ -1,7 +1,16 @@
-import {View, Text, SafeAreaView, Image} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
 
 const Login = () => {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
   return (
     <SafeAreaView style={{backgroundColor: 'lightblue'}}>
       <View style={{alignItems: 'center', padding: 10, marginTop: 50}}>
@@ -10,13 +19,72 @@ const Login = () => {
           style={{width: 250, height: 250}}
         />
       </View>
-      <View style={{backgroundColor: 'white',borderTopLeftRadius:50,borderTopRightRadius:50}}>
+      <View
+        style={{
+          backgroundColor: 'white',
+          borderTopLeftRadius: 50,
+          borderTopRightRadius: 50,
+        }}>
         <View style={{paddingTop: 30, alignItems: 'center'}}>
-          <Text style={{fontSize: 20,fontWeight:'800'}}>Employee Login</Text>
+          <Text style={{fontSize: 20, fontWeight: '800'}}>Employee Login</Text>
+        </View>
+        <View style={{padding: 20}}>
+          <View style={{marginBottom: 20}}>
+            <Text style={{marginBottom: 5, fontSize: 18}}>Mail:</Text>
+            <TextInput
+              style={{
+                borderBottomWidth: 1,
+                borderColor: 'gray',
+                fontSize: 18,
+              }}
+              placeholder="Enter your mail"
+            />
+          </View>
+
+          <View style={{marginBottom: 20}}>
+            <Text style={{marginBottom: 5, fontSize: 18}}>Password:</Text>
+            <View
+              style={{
+                borderBottomWidth: 1,
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}>
+              <TextInput
+                style={{
+                  width: '90%',
+                  fontSize: 18,
+                }}
+                placeholder="Enter your password"
+                secureTextEntry={!passwordVisible}
+              />
+              <TouchableOpacity
+                onPress={() => setPasswordVisible(!passwordVisible)}>
+                <Image
+                  style={{width: 25, height: 25}}
+                  source={
+                    passwordVisible
+                      ? require('../../assets/Eye.png')
+                      : require('../../assets/Eyeoff.png')
+                  }
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+          <TouchableOpacity style={{marginBottom: 20, alignItems:'flex-end'}}>
+            <Text style={{marginBottom: 5, fontSize:18, color:'blue',borderBottomWidth:1,borderColor:'blue'}}>Forgoot Paswword</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              backgroundColor: '#3b56dc',
+              alignItems: 'center',
+              padding: 20,
+              borderRadius: 50,
+            }}>
+            <Text style={{color: 'white', fontSize: 18}}>Login</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </SafeAreaView>
   );
 };
-
 export default Login;
